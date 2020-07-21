02b7493956df9588f34833fa033ae0ef83a8e156
SoftcornerBebras <softcornerbebras@gmail.co>
2020-07-21 22:22:42 +0530

    latest21-07

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1dc7b8c62a9fdb13e0843646e7c5f118.js"
+  "/precache-manifest.4ede927be4772bd20d3149133bcd868a.js"
 );
 
 self.addEventListener('message', (event) => {
